5cee6e6aa0627831e61b7efa235cea683523a854
plain-phw <s2028@e-mirim.hs.kr>
2022-08-23 23:32:23 +0900

    [End] Memo Project

--- Memo/App.js
+++ Memo/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import { Button, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [txt, setTxt] = useState('안녕하세요.');
+
+  useEffect(() => {
+    console.log('프로그램 시작');
+  }, []);
+
+  const saveData = async (value) => {
+    try {
+      await AsyncStorage.setItem('assa', value)
+      console.log('저장');
+    } catch (e) {
+      // Saving error
+    }
+  }
+
+  const loadData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('assa')
+      if(value !== null) {
+        setTxt(value);
+      }
+    } catch (e) {
+      // error reading value
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex:1, }}> 
+      <SafeAreaView style={{ flex:1, }}>
+        <StatusBar style={{flex:1,}}/>
+        <View style={{ padding:10, flexDirection: 'row', 
+                      alignItems: 'center', justifyContent: 'space-between', }}>
+          <Button title='저장' onPress={() => saveData(txt)} />
+          <Text style={{ textAlign: 'center', fontSize: 18, }}>메모장</Text>
+          <Button title='불러오기' onPress={() => loadData()}/>
+        </View>
+        
+        <View style={{backgroundColor: '#eeeeee', flex: 1, padding: 10, }}>
+          <TextInput value={txt} 
+            onChangeText={txt => setTxt(txt)} 
+            multiline
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
